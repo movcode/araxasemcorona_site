2a7493f8d1cbe3ac5878e604e9ee5640aceabd20
Rafael Augusto <movcode@gmail.com>
2020-04-17 03:36:55 -0300

    implement filter input

--- src/components/establishment/establishment.js
+++ src/components/establishment/establishment.js
@@ -6,14 +6,17 @@ import Center from '../share_components/center';
 import SectorItens from './sectors';
 import CategoriesItens from './categories';
 import ListEstablishment from './list'
+import { BoxEstablishments, InputSearch, IconSearch } from './style';
 
 const Establishment = ({ sectors }) => {
 
     const [categories, _categories] = useState(false);
     const [establishments, _establishments] = useState(false);
 
+    const [establishmentFiltered, _establishmentFiltered] = useState(false);
+
     useEffect(() => {
-        sectors && sectors[0]&&
+        sectors && sectors[0] &&
             _categories(sectors[0].categories);
     }, [sectors]);
 
@@ -29,6 +32,14 @@ const Establishment = ({ sectors }) => {
         }
     }, [establishments, sectors]);
 
+
+    const filter = text => {
+        const filtered = establishments.filter(e =>
+            e.name.toLowerCase().indexOf(text.toLowerCase()) >= 0
+        );
+        _establishmentFiltered(filtered);
+
+    }
 
     return (
         <Background bg="white" bottom="120">
@@ -53,25 +64,33 @@ const Establishment = ({ sectors }) => {
                             _establishments={_establishments} />
                     </div>
                 </Center>
+            </Container>
 
+            <BoxEstablishments className="col-md-12">
                 <Center>
-                    <div className="col-md-12">
-                        {
-                            establishments !== "empty" && establishments.length > 0
-                                ? <ListEstablishment establishments={establishments} />
-                                : <div style={{ marginTop: "60px" }}>
-                                    <Center>
-                                        <Text color="black">Nenhum estabelecimento cadastrado nesta categoria. </Text>
-                                    </Center>
-                                    <Center>
-                                        <Link to="/registro"
-                                            className="btn btn-warning  btn-sm">Clique aqui para adicionar um estabelecimento</Link>
-                                    </Center>
-                                </div>
-                        }
+                    <div className=" col-md-4">
+                        <InputSearch onChange={(e) => filter(e.target.value)}
+                            className="form-control" type="text" placeholder="Buscar " />
+                        <IconSearch className="ti-search" />
                     </div>
                 </Center>
-            </Container>
+
+                {
+                    establishments !== "empty" && establishments.length > 0
+                        ? establishmentFiltered ? <ListEstablishment establishments={establishmentFiltered} />
+                            : <ListEstablishment establishments={establishments} />
+                        : <div style={{ marginTop: "60px" }}>
+                            <Center>
+                                <Text color="black">Nenhum estabelecimento cadastrado nesta categoria. </Text>
+                            </Center>
+                            <Center>
+                                <Link to="/registro"
+                                    className="btn btn-warning  btn-sm">Clique aqui para adicionar um estabelecimento</Link>
+                            </Center>
+                        </div>
+                }
+            </BoxEstablishments>
+
         </Background>
     );
 
---
+++ src/components/establishment/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const BoxEstablishments = styled.div`
+
+margin-top:60px;
+@media(min-width:1024px){
+    margin-left:30px;
+}
+`;
+
+export const InputSearch = styled.input`
+position:relative;
+z-index:2;
+`;
+
+export const IconSearch = styled.div`
+position:relative;
+height:40px;
+line-height:40px;
+margin-right:10px;
+color:#C2C2C2;
+font-weight:bold;
+float:right;
+top:-40px;
+z-index:2;
+`;
